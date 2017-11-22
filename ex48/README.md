ef36b1d90d603fe5654d6c289a094bc427a3f67f
Stephen Borutta <33042374+SteviBee@users.noreply.github.com>
2017-11-22 11:12:15 -0600

    updated readme in ex48 and added ex49

--- ex48/README.md
+++ ex48/README.md
@@ -1,9 +1,2 @@
-To use this Skeleton:
-
-1. Make a copy of your skeleton directory. Name it after your new project.
-2. Rename (move) the NAME module to be the name of your project or whatever you want to call your root module.
-3. Edit your setup.py to have all the information for your project.
-4. Rename tests/NAME_tests.py to also have your module name.
-5. Double check it's all working using nosetests again.
-    check via powershell "python -m nose"
-6. Start coding.
+# Lexicon
+Takes input and turns if it matches a list of nouns, stop words, directions, or verbs it adds a tag to it identifying it. 
--- ex49/setup.py
+++ ex49/setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description' : 'Making Sentences',
+    'description' : 'My Project',
     'author' : 'Stephen Borutta',
-    'url' : 'https://github.com/SteviBee/Learn-Python-The-Hard-Way/tree/master/ex48',
+    'url' : 'URL to get it all at',
     'download_url' : 'Where to download it',
     'author_email' : 'boruttasp@gmail.com',
     'version' : '0.1', #major.minor[.patch[.sub]]
     'install_requires' : ['nose'],
     'packages' : ['NAME'],
     'scripts' : [],
-    'name' : 'lexicon'
+    'name' : 'projectname'
 }
 
 setup(**config)
